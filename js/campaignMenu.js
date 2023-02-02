c5488cf13042a7852b1559c1bfb11c1e7425bc8c
Jnxng <s10222060@connect.np.edu.sg>
2023-02-02 15:17:14 +0800

    creating campaign files and html work

---
+++ js/campaignMenu.js
@@ -0,0 +1,15 @@
+function campaignStage3(){
+    window.location = "campaignStage1.html";
+}
+
+function campaignStage2(){
+    window.location = "campaignStage2.html";
+}
+
+function campaignStage3(){
+    window.location = "campaignStage3.html";
+}
+
+function back(){
+    window.history.back();
+}
