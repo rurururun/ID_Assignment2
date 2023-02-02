cca106c3322a34d8df74984c4b8ea95b7c662d83
Jnxng <s10222060@connect.np.edu.sg>
2023-02-02 15:39:40 +0800

    editing the campagin page

--- js/campaignMenu.js
+++ js/campaignMenu.js
@@ -9,6 +9,12 @@ function campaignStage2(){
 function campaignStage3(){
     window.location = "campaignStage3.html";
 }
+function campaignStage4(){
+    window.location = "campaignStage4.html";
+}
+function campaignStage5(){
+    window.location = "campaignStage5.html";
+}
 
 function back(){
     window.history.back();
