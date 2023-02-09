30e993d380af121551ae5c3c78e09dc9373d03ff
Chua Guo Heng <guohengchua@gmail.com>
2023-02-09 21:15:12 +0800

    Added in a legend to the game

--- js/options.js
+++ js/options.js
@@ -1,12 +1,26 @@
 import { getInputDirection } from "./input.js";
 
 export let gamePaused = false;
+let gameMenuOpened = false;
 
 let inputDirection;
+
+$("#gameMenu").hide();
+
+$("#legend").click(function(){
+    if (!gameMenuOpened){
+        gamePaused = true;
+        gameMenuOpened = true;
+        $("#gameMenu").show();
+    }
+})
 
 window.addEventListener('keyup', e => {
     switch (e.key){
         case 'p':
+            if (gameMenuOpened){
+                break;
+            }
             inputDirection = getInputDirection();
             if (gamePaused && (inputDirection.x != 0 || inputDirection.y != 0)){
                 gamePaused = false;
@@ -18,7 +32,17 @@ window.addEventListener('keyup', e => {
             }
             break;
         case 'Escape':
+            if (gameMenuOpened){
+                gamePaused = false;
+                gameMenuOpened = false;
+                $("#gameMenu").hide();
+                break;
+            }
             window.location = "mainMenu.html";
             break;
     }
 })
+
+export function changeGameState(newState){
+    gamePaused = newState;
+}
